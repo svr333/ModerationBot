1034f131d50933d9f80030808c0f5cf8d30cce05
Senne <vanrompaey.senne@protonmail.com>
2021-05-11 21:17:35 +0200

    fix hierarchy check

--- src/AdvancedBot.Core/Commands/Preconditions/RequireHigherHierarchy.cs
+++ src/AdvancedBot.Core/Commands/Preconditions/RequireHigherHierarchy.cs
@@ -3,28 +3,46 @@ using System.Linq;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
+using Discord.Rest;
 using Discord.WebSocket;
 
 namespace AdvancedBot.Core.Commands.Preconditions
 {
     public class RequireHigherHierarchyPrecondition : ParameterPreconditionAttribute
     {
-        public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, ParameterInfo parameter, object value, IServiceProvider services)
+        public async override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, ParameterInfo parameter, object value, IServiceProvider services)
         {
-            if (!(value is SocketGuildUser user))
+            var ctx = context as SocketCommandContext;
+            var guild = await ctx.Client.Rest.GetGuildAsync(ctx.Guild.Id);
+            var user = await guild.GetUserAsync((value as IUser).Id);
+            var bot = await guild.GetUserAsync(context.Client.CurrentUser.Id);
+
+            var userHierarchy = GetHierarchyOfUser(guild, user);
+
+            if (guild.Roles.OrderByDescending(r => r.Position).FirstOrDefault().Position <= userHierarchy)
             {
-                return Task.FromResult(PreconditionResult.FromError("You must mention a valid user."));
+                return PreconditionResult.FromError("The bot must be ranked higher than the mentioned user.");
             }
 
-            var bot = (SocketGuildUser)context.Client.CurrentUser;
-            if (bot.Guild.Roles.OrderByDescending(r => r.Position).FirstOrDefault().Position <= user.Hierarchy)
+            return ((context.User as SocketGuildUser).Hierarchy > userHierarchy)
+                    ? PreconditionResult.FromSuccess()
+                    : PreconditionResult.FromError("You must be ranked higher than the mentioned user.");
+        }
+
+        private int GetHierarchyOfUser(RestGuild guild, RestGuildUser user)
+        {
+            var orderedRoles = guild.Roles.OrderByDescending(r => r.Position).ToArray();
+            var orderedRoleIds = orderedRoles.Select(x => x.Id).ToArray();
+
+            for (int i = 0; i < orderedRoleIds.Length; i++)
             {
-                return Task.FromResult(PreconditionResult.FromError("The bot must be ranked higher than the mentioned user."));
+                var test = user.RoleIds.FirstOrDefault(x => x == orderedRoleIds[i]);
+
+                if (test != 0)
+                    return orderedRoles[i].Position;
             }
 
-            return ((context.User as SocketGuildUser).Hierarchy > user.Hierarchy)
-                    ? Task.FromResult(PreconditionResult.FromSuccess())
-                    : Task.FromResult(PreconditionResult.FromError("You must be ranked higher than the mentioned user."));
+            return 0;
         }
     }
 }
